1936ea6243b8d9e10a5ba69b24dd30a31dac59ae
Ravindra Singh <ravindra.singh@ihsmarkit.com>
2019-03-17 16:22:10 +0530

    vRNI Code First commit

---
+++ vrniAuthToken.py
@@ -0,0 +1,62 @@
+#########################################################################################################################################
+##                              Code purpose: Login to vRNI and update the data sources                                                ##
+##                              Code Written By: Ravindra Singh                                                                        ##
+##                              Code State : Test                                                                                      ##
+##                              Last Prod Run : N/A                                                                                    ##
+#########################################################################################################################################
+
+
+
+#!/usr/bin/python
+
+
+import requests, os
+import json, time
+
+
+# Global Variable used:
+
+
+
+def GenrateToken():
+	
+	BaseUrl = "https://vwcp9vcsvrni01.info.corp/api/ni/auth/token"
+	creds = {
+	"username": os.environ.get('vrni_user'),
+	"password" : os.environ.get('A_PWD'),
+	"domain": {
+		"domain_type": "LDAP",
+		"value":"info.corp"
+		}
+	}
+	headers = {'Content-Type':'application/json'}
+	resp =  requests.post(BaseUrl,headers=headers, data = json.dumps(creds), verify = False,)
+	AuthKey = resp.json()['token']
+	print AuthKey
+	return AuthKey
+
+
+
+def login(key):
+	AuthKey = key
+	BaseUrl = "https://vwcp9vcsvrni01.info.corp/api/ni"
+	Uri = "/data-sources/cisco-switches"
+	headers = {
+	"Content-Type":"application/json",
+	"NetworkInsight_AuthKey":AuthKey
+	}
+	CiscoResp = requests.get((BaseUrl+Uri),headers = headers, verify=False)
+	print CiscoResp.text
+
+
+
+
+
+
+# 1.) GenrateToken() function will generate a session AuthToken which can be used to consume vRNI APIs
+AuthKey = GenrateToken()
+
+# 2.) login(AuthKey) is in test phase and is listing the cisco switchs
+login(AuthKey)
+
+
